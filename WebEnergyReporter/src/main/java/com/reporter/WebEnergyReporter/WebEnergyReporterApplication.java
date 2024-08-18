32c6822bc88a1322e675522867bbe63c4f2bdbb1
deprotate <gribozui.top@gmail.com>
2024-08-18 20:21:41 +0300

    August_eighteen

--- WebEnergyReporter/src/main/java/com/reporter/WebEnergyReporter/WebEnergyReporterApplication.java
+++ WebEnergyReporter/src/main/java/com/reporter/WebEnergyReporter/WebEnergyReporterApplication.java
@@ -1,13 +1,13 @@
-package com.example.NumTest;
+package com.reporter.WebEnergyReporter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NumTestApplication {
+public class WebEnergyReporterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NumTestApplication.class, args);
+		SpringApplication.run(WebEnergyReporterApplication.class, args);
 	}
 
 }
--- WebEnergyReporter/src/test/java/com/reporter/WebEnergyReporter/WebEnergyReporterApplicationTests.java
+++ WebEnergyReporter/src/test/java/com/reporter/WebEnergyReporter/WebEnergyReporterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.NumTest;
+package com.reporter.WebEnergyReporter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NumTestApplicationTests {
+class WebEnergyReporterApplicationTests {
 
 	@Test
 	void contextLoads() {
